732ffb81d1bbad693b1c48f781ab4239188c7973
Svetoslav Georgiev <sv.georgiev23@gmail.com>
2021-02-24 19:06:45 +0200

    adding picture copy task

---
+++ file_tasks/file_copy/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File original = new File("picture.jpg");
+        File copy = new File("copy.jpg");
+        try {
+            if(!copy.exists()) {
+                copy.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try(
+                FileInputStream fis = new FileInputStream(original);
+                FileOutputStream fos = new FileOutputStream(copy);
+        ){
+            int b  = 0;
+            while(b!=-1){
+                b = fis.read();
+                if(b==-1){
+                    break;
+                }
+                fos.write(b);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
